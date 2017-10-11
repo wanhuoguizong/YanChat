43bd7ef39ea06d8f093ad9cb4a0eb1fdeb5e9058
weiwanyan <weiwanyan@entrobus.com>
2017-10-11 16:09:04 +0800

    新建

---
+++ client/src/main/java/com/xinlingyijiu/yanchat/client/Demo.java
@@ -0,0 +1,7 @@
+package com.xinlingyijiu.yanchat.client;
+
+/**
+ * Created by laotou on 2017/10/11.
+ */
+public class Demo {
+}
---
+++ core/src/main/java/com/xinlingyijiu/yanchat/util/CloseableUtil.java
@@ -0,0 +1,18 @@
+package com.xinlingyijiu.yanchat.util;
+
+import java.io.Closeable;
+import java.io.IOException;
+
+/**
+ * Created by laotou on 2017/10/11.
+ */
+public class CloseableUtil {
+    public static void close(Closeable closeable) {
+        if (closeable == null) return;
+        try {
+            closeable.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
