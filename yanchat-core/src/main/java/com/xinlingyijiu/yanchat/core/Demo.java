8e662c06549803e1e2e0422e390895d51a2026df
weiwanyan <weiwanyan@entrobus.com>
2017-10-11 17:02:25 +0800

    ddd

--- yanchat-core/src/main/java/com/xinlingyijiu/yanchat/core/Demo.java
+++ yanchat-core/src/main/java/com/xinlingyijiu/yanchat/core/Demo.java
@@ -4,4 +4,7 @@ package com.xinlingyijiu.yanchat.core;
  * Created by laotou on 2017/10/11.
  */
 public class Demo {
+    public static void main(String[] args) {
+        System.out.println("Æô¶¯£¡");
+    }
 }
